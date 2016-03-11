ffa1c73c783062ef8901786599b8715a5cb0535e
anthony <azhang9@gmail.com>
2016-03-11 17:45:41 -0500

    Update aggregate values

--- test/test_fill_database.py
+++ test/test_fill_database.py
@@ -22,8 +22,8 @@ class TestStringMethods(unittest.TestCase):
         self.sc.stop()
 
     def test_length(self):
-        self.assertEqual(self.raw_pings.count(), 4096)
-        self.assertEqual(len(self.crash_rate_aggregates), 1024)
+        self.assertEqual(self.raw_pings.count(), 8192)
+        self.assertEqual(len(self.crash_rate_aggregates), 4096)
 
     def test_keys(self):
         for keys, crashes in self.crash_rate_aggregates:
@@ -31,20 +31,22 @@ class TestStringMethods(unittest.TestCase):
             self.assertTrue(re.match("^\d+(?:\.\d+(?:[a-z]\d+)?)?$", keys[1]), keys[1]) # version
             self.assertTrue(re.match("^\d{14}$", keys[2]), keys[2]) # build ID
             self.assertIn(keys[3], {"nightly", "aurora", "beta", "release"}) # channel
-            self.assertIn(keys[4], {"Linux", "Windows_NT", "Darwin"}) # OS name
-            self.assertTrue(re.match("^[\d\.]+$", keys[5]), keys[5]) # OS version
-            self.assertTrue(re.match("^[\w-]+$", keys[6]), keys[6]) # locale
-            self.assertTrue(keys[7] is None or "@" in keys[7], keys[7]) # experiment ID
-            self.assertTrue(re.match("^[\w-]+$", keys[8]), keys[8]) # experiment branch
-            self.assertTrue(isinstance(keys[9], bool), keys[9]) # E10S enabled
+            self.assertIn(keys[4], {"Firefox", "Fennec"}) # application
+            self.assertIn(keys[5], {"Linux", "Windows_NT", "Darwin"}) # OS name
+            self.assertTrue(re.match("^[\d\.]+$", keys[6]), keys[6]) # OS version
+            self.assertTrue(re.match("^[\w-]+$", keys[7]), keys[7]) # architecture
+            self.assertTrue(re.match("^[\w-]+$", keys[8]), keys[8]) # country
+            self.assertTrue(keys[9] is None or "@" in keys[9], keys[9]) # experiment ID
+            self.assertTrue(re.match("^[\w-]+$", keys[10]), keys[10]) # experiment branch
+            self.assertTrue(isinstance(keys[11], bool), keys[11]) # E10S enabled
 
     def test_crash_rates(self):
         for keys, crashes in self.crash_rate_aggregates:
             usage_hours, main_crashes, content_crashes, plugin_crashes = crashes
-            self.assertEqual(usage_hours, 42 * 4 / 3600.0)
-            self.assertEqual(main_crashes, 2)
-            self.assertEqual(content_crashes, 42 * 4)
-            self.assertEqual(plugin_crashes, 42 * 8)
+            self.assertEqual(usage_hours, 42 * 2 / 3600.0)
+            self.assertEqual(main_crashes, 1)
+            self.assertEqual(content_crashes, 42 * 2)
+            self.assertEqual(plugin_crashes, 42 * 4)
 
 if __name__ == '__main__':
     unittest.main()
