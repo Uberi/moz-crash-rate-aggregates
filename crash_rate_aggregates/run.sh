856d1a7229d80a69f0b29f85b5c37a1f265f1d15
anthony <azhang9@gmail.com>
2016-03-15 16:50:38 -0400

    Better organization

--- crash_rate_aggregates/run.sh
+++ crash_rate_aggregates/run.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
-# set up environment
-sudo apt-get install libsnappy-dev liblzma-dev python-psycopg2
+<<<ENDCOMMENT
+# these setup routines are used to set up an environment when non is available, like in Vagrant
+# since the script usually runs on the Telemetry Spark Analysis environment, the requirements should already be installed
+
+# install packages
+sudo apt-get install libsnappy-dev liblzma-dev python-psycopg2 openjdk-7-jdk
 sudo pip2 install telemetry-tools
+sudo apt-get install python-numpy python-pandas # these are dependencies of python_moztelemetry, but installing them with apt-get massively speeds up python_moztelemetry installation
 sudo pip2 install python_moztelemetry
+
+# install Spark
+wget http://d3kbcqa49mib13.cloudfront.net/spark-1.3.1-bin-hadoop2.4.tgz -O spark-hadoop.tgz
+tar -xzf spark-hadoop.tgz
+rm spark-hadoop.tgz
+export SPARK_HOME=$(pwd)/spark-hadoop
+export PYTHONPATH=$SPARK_HOME/python
+ENDCOMMENT
 
 # get DB password from S3 (requires AWS credentials)
 aws s3 cp s3://telemetry-spark-emr-2/crash_rate_aggregates_credentials crash_rate_aggregates_credentials
