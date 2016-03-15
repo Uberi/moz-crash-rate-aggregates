b60e18c277338f748dce9354772ff0f22d5e1f28
anthony <azhang9@gmail.com>
2016-03-15 14:56:21 -0400

    Partitioning, use JSON columns, further setup improvements, documentation

--- crash_rate_aggregates/fill_database.py
+++ crash_rate_aggregates/fill_database.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
 import sys
-from datetime import datetime
+from datetime import datetime, date
 
 import psycopg2
+from psycopg2 import extras
 import numpy as np
 
 from moztelemetry.spark import get_pings, get_pings_properties
 
 FRACTION = 0.1
 
+# paths/dimensions within the ping to compare by, in the same format as the second parameter to `get_pings_properties`
+# in https://github.com/mozilla/python_moztelemetry/blob/master/moztelemetry/spark.py
 COMPARABLE_DIMENSIONS = [
     "environment/build/version",
     "environment/build/buildId",
@@ -23,6 +26,8 @@ COMPARABLE_DIMENSIONS = [
     "environment/addons/activeExperiment/branch",
     "environment/settings/e10sEnabled",
 ]
+
+# names of the comparable dimensions above, used as dimension names in the database
 DIMENSION_NAMES = [
     "build_version",
     "build_id",
@@ -38,7 +43,7 @@ DIMENSION_NAMES = [
 ]
 assert len(COMPARABLE_DIMENSIONS) == len(DIMENSION_NAMES)
 
-def compare_crashes(pings, comparable_dimensions):
+def compare_crashes(pings, comparable_dimensions, dimension_names):
     """Returns a PairRDD where keys are user configurations and values are Numpy arrays of the form [usage hours, main process crashes, content process crashes, plugin crashes]"""
     ping_properties = get_pings_properties(pings, comparable_dimensions + [
         "payload/info/subsessionLength",
@@ -48,9 +53,10 @@ def compare_crashes(pings, comparable_dimensions):
         "payload/keyedHistograms/SUBPROCESS_ABNORMAL_ABORT/plugin",
         "payload/keyedHistograms/SUBPROCESS_ABNORMAL_ABORT/gmplugin",
     ])
-    return ping_properties.map(lambda p: (
+    crash_values = ping_properties.map(lambda p: (
         # the keys we want to filter based on
         (p["meta/submissionDate"],) + tuple(p[key] for key in comparable_dimensions),
+        # the crash values
         np.array([
             (p["payload/info/subsessionLength"] or 0) / 3600.0,
             int(p["meta/reason"] == "aborted-session"), # main process crashes
@@ -59,6 +65,20 @@ def compare_crashes(pings, comparable_dimensions):
             (p["payload/keyedHistograms/SUBPROCESS_ABNORMAL_ABORT/gmplugin"] or 0) # plugin crashes
         ])
     )).reduceByKey(lambda a, b: a + b)
+
+    def dimension_mapping(pair):
+        dimension_key, aggregates = pair
+        submission_date, dimension_values = dimension_key[0], dimension_key[1:]
+        submission_date = datetime.strptime(submission_date, "%Y%m%d") # convert the YYYYMMDD format to a real datetime
+        return (
+            submission_date,
+            {
+                key: dimension_value
+                for key, dimension_value in zip(dimension_names, dimension_values)
+            },
+            aggregates, # crash values
+        )
+    return crash_values.map(dimension_mapping)
 
 def retrieve_crash_data(sc, submission_date_range, comparable_dimensions, fraction = 0.1):
     # get the raw data
@@ -87,62 +107,102 @@ if __name__ == "__main__":
     from pyspark import SparkContext
 
     today_utc = datetime.utcnow().strftime("%Y%m%d")
-    parser = argparse.ArgumentParser(description="Fill an Amazon RDB instance with crash rate aggregates for a certain date range")
+    parser = argparse.ArgumentParser(description="Fill a Postgresql database with crash rate aggregates for a certain date range.")
     parser.add_argument("--min-submission-date", help="Earliest date to include in the aggregate calculation in YYYYMMDD format (defaults to the current UTC date)", default=today_utc)
     parser.add_argument("--max-submission-date", help="Latest date to include in the aggregate calculation in YYYYMMDD format (defaults to the current UTC date)", default=today_utc)
-    parser.add_argument("--aws-rdb-host", help="Public DNS/address of the Amazon RDB instance", required=True)
-    parser.add_argument("--aws-rdb-name", help="Name of the Amazon RDB instance database", required=True)
-    parser.add_argument("--aws-rdb-username", help="Username for the Amazon RDB instance", required=True)
-    parser.add_argument("--aws-rdb-password", help="Password for the Amazon RDB instance", required=True)
+    parser.add_argument("--pg-host", help="Host/address of the Postgresql database", required=True)
+    parser.add_argument("--pg-name", help="Name of the Postgresql database", required=True)
+    parser.add_argument("--pg-username", help="Username for the Postgresql database", required=True)
+    parser.add_argument("--pg-password", help="Password for the Postgresql database", required=True)
     args = parser.parse_args()
+    args.min_submission_date, args.max_submission_date = "20160301", "20160630"
     SUBMISSION_DATE_RANGE = (args.min_submission_date, args.max_submission_date)
-    DB_HOST, DB_NAME, DB_USER, DB_PASS = args.aws_rdb_host, args.aws_rdb_name, args.aws_rdb_username, args.aws_rdb_password
+    DB_HOST, DB_NAME, DB_USER, DB_PASS = args.pg_host, args.pg_name, args.pg_username, args.pg_password
 
+    start_date = datetime.strptime(SUBMISSION_DATE_RANGE[0], "%Y%m%d").date()
+    end_date = datetime.strptime(SUBMISSION_DATE_RANGE[1], "%Y%m%d").date()
+
+    # get a representative sample of saved-session and aborted-session pings
     sc = SparkContext()
     pings = retrieve_crash_data(sc, SUBMISSION_DATE_RANGE, COMPARABLE_DIMENSIONS, FRACTION)
+
+    # useful statements for testing the program
+    #sc = SparkContext(master="local[1]") # run sequentially with only 1 worker
+    #import sys, os; sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "test")); import dataset; pings = sc.parallelize(list(dataset.generate_pings())) # use test pings; very good for testing queries
+
+    # compare crashes by all of the above dimensions
+    result = compare_crashes(pings, COMPARABLE_DIMENSIONS, DIMENSION_NAMES)
 
     conn = psycopg2.connect(host=DB_HOST, database=DB_NAME, user=DB_USER, password=DB_PASS)
     cur = conn.cursor()
 
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS aggregates (
+    CREATE TABLE IF NOT EXISTS crash_aggregates (
         id serial PRIMARY KEY,
-        submission_date date,
-        build_version varchar,
-        build_id varchar,
-        channel varchar,
-        application varchar,
-        os_name varchar,
-        os_version varchar,
-        architecture varchar,
-        country varchar,
-        experiment_id varchar,
-        experiment_branch varchar,
-        e10s_enabled varchar,
-        usage_hours real,
-        main_crashes real,
-        content_crashes real,
-        plugin_crashes real
+        submission_date date NOT NULL,
+        dimensions jsonb,
+        usage_hours real NOT NULL,
+        main_crashes real NOT NULL,
+        content_crashes real NOT NULL,
+        plugin_crashes real NOT NULL
     );
+    """)
+
+    # create child tables that inherit from the crash_aggregates table; these partition the data by month for faster querying
+    # when running queries, we can still select from the parent crash_aggregates table; the query will use the child tables as needed
+    current_month = date(start_date.year, start_date.month, 1)
+    while current_month <= end_date: # loop through the month range
+        next_month = date(current_month.year, (current_month.month % 12) + 1, 1)
+        cur.execute("""
+        DO $$
+        BEGIN
+        IF NOT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}') THEN
+            CREATE TABLE {table_name} (
+                CONSTRAINT {table_name}_pk PRIMARY KEY (id),
+                CONSTRAINT {table_name}_ck CHECK (submission_date >= DATE '{current_month}' AND submission_date < DATE '{next_month}')
+            ) INHERITS (crash_aggregates);
+            CREATE INDEX {table_name}_date_idx ON {table_name} (submission_date);
+            CREATE INDEX {table_name}_dimension_idx ON {table_name} USING gin (dimensions);
+        END IF;
+        END
+        $$
+        """.format(
+            table_name="crash_aggregates_partition_{}_{}".format(current_month.year, current_month.month),
+            current_month=datetime.strftime(current_month, "%Y-%m-%d"),
+            next_month=datetime.strftime(next_month, "%Y-%m-%d"),
+        ))
+        current_month = next_month
+
+    # when we attempt to insert into crash_aggregates, redirect it to the proper child table instead
+    # the child table will only store a month's worth of data (~3000000 rows), so queries that only need
+    # a specific submission date range will have much less to look through
+    cur.execute("""
+    CREATE OR REPLACE FUNCTION crash_aggregates_insert_trigger()
+    RETURNS TRIGGER AS $$
+    BEGIN
+        EXECUTE format('INSERT INTO %I SELECT $1.*', 'crash_aggregates_partition_' || EXTRACT(YEAR FROM NEW.submission_date) || '_' || EXTRACT(MONTH FROM NEW.submission_date)) USING NEW;
+        RETURN NEW;
+    END;
+    $$
+    LANGUAGE plpgsql;
+
+    DROP TRIGGER IF EXISTS insert_crash_aggregates_trigger ON crash_aggregates;
+    CREATE TRIGGER insert_crash_aggregates_trigger BEFORE INSERT ON crash_aggregates FOR EACH ROW EXECUTE PROCEDURE crash_aggregates_insert_trigger();
     """)
 
     # remove previous data for the selected days, if available
     # this is necessary to be able to backfill data properly
-    cur.execute(
-        """DELETE FROM aggregates WHERE submission_date >= %s and submission_date <= %s""".format(", ".join(DIMENSION_NAMES)),
-        (datetime.strptime(SUBMISSION_DATE_RANGE[0], "%Y%m%d").date(), datetime.strptime(SUBMISSION_DATE_RANGE[1], "%Y%m%d").date())
-    )
+    cur.execute("""DELETE FROM crash_aggregates WHERE submission_date >= %s and submission_date <= %s""", (start_date, end_date))
 
-    result = compare_crashes(pings, COMPARABLE_DIMENSIONS)
-    for dimension_values, crash_data in result.toLocalIterator():
-        submission_date, dimension_values = dimension_values[0], dimension_values[1:]
-        submission_date = datetime.strptime(submission_date, "%Y%m%d")
+    for submission_date, dimension_values, crash_data in result.toLocalIterator():
         usage_hours, main_crashes, content_crashes, plugin_crashes = crash_data
         cur.execute(
-            """INSERT INTO aggregates(submission_date, {}, usage_hours, main_crashes, content_crashes, plugin_crashes) VALUES (%s, {}%s, %s, %s, %s)""".format(
-                ", ".join(DIMENSION_NAMES), "%s, " * len(DIMENSION_NAMES)
-            ),
-            (submission_date,) + dimension_values + (usage_hours, main_crashes, content_crashes, plugin_crashes)
+            """INSERT INTO crash_aggregates(submission_date, dimensions, usage_hours, main_crashes, content_crashes, plugin_crashes) VALUES (%s, %s, %s, %s, %s, %s)""",
+            (
+                submission_date,
+                extras.Json(dimension_values),
+                usage_hours, main_crashes, content_crashes, plugin_crashes
+            )
         )
 
     conn.commit()
--- crash_rate_aggregates/run.sh
+++ crash_rate_aggregates/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# set up environment
+sudo apt-get install libsnappy-dev liblzma-dev
+sudo pip2 install telemetry-tools
+sudo pip2 install python_moztelemetry
+
 # get DB password from S3 (requires AWS credentials)
 aws s3 cp s3://telemetry-spark-emr-2/crash_rate_aggregates_credentials crash_rate_aggregates_credentials
 DB_PASS=$(python -c 'import json;print json.load(open("crash_rate_aggregates_credentials", "r"))["password"]')
@@ -10,5 +15,5 @@ DB_NAME=crash_rate_aggregates
 DB_USER=root
 DB_HOST=crash-rate-aggregates.cppmil15uwvg.us-west-2.rds.amazonaws.com
 python fill_database.py \
-  --aws-rdb-host "$DB_HOST" --aws-rdb-name "$DB_NAME" \
-  --aws-rdb-username "$DB_USER" --aws-rdb-password "$DB_PASS"
+  --pg-host "$DB_HOST" --pg-name "$DB_NAME" \
+  --pg-username "$DB_USER" --pg-password "$DB_PASS"
