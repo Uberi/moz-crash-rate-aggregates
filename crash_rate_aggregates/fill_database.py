15e2e3afec7717f8c4dd634c43712b9a006830c0
Anthony Zhang <azhang9@gmail.com>
2016-04-06 19:08:21 -0400

    Avoid using get_pings_properties as suggested

--- crash_rate_aggregates/fill_database.py
+++ crash_rate_aggregates/fill_database.py
@@ -8,25 +8,24 @@ import numpy as np
 import dateutil.parser
 import pyspark.sql.types as types
 
-from moztelemetry.spark import get_pings, get_pings_properties
+from moztelemetry.spark import get_pings, Histogram
 from datetime import datetime, date, timedelta
 
 FRACTION = 1.0
 
-# paths/dimensions within the ping to compare by, in the same format as the second parameter to `get_pings_properties`
-# in https://github.com/mozilla/python_moztelemetry/blob/master/moztelemetry/spark.py
+# paths/dimensions within the ping to compare by
 COMPARABLE_DIMENSIONS = [
-    "environment/build/version",
-    "environment/build/buildId",
-    "application/channel",
-    "application/name",
-    "environment/system/os/name",
-    "environment/system/os/version",
-    "environment/build/architecture",
-    "meta/geoCountry",
-    "environment/addons/activeExperiment/id",
-    "environment/addons/activeExperiment/branch",
-    "environment/settings/e10sEnabled",
+    ("environment", "build", "version"),
+    ("environment", "build", "buildId"),
+    ("application", "channel"),
+    ("application", "name"),
+    ("environment", "system", "os", "name"),
+    ("environment", "system", "os", "version"),
+    ("environment", "build", "architecture"),
+    ("meta", "geoCountry"),
+    ("environment", "addons", "activeExperiment", "id"),
+    ("environment", "addons", "activeExperiment", "branch"),
+    ("environment", "settings", "e10sEnabled"),
 ]
 
 # names of the comparable dimensions above, used as dimension names in the database
@@ -43,33 +42,45 @@ DIMENSION_NAMES = [
     "experiment_branch",
     "e10s_enabled",
 ]
-assert len(COMPARABLE_DIMENSIONS) == len(DIMENSION_NAMES)
+
+assert len(COMPARABLE_DIMENSIONS) == len(DIMENSION_NAMES), "COMPARABLE_DIMENSIONS and DIMENSION_NAMES must match"
 
 def compare_crashes(pings, start_date, end_date, comparable_dimensions, dimension_names):
     """Returns a PairRDD where keys are user configurations and values are Numpy arrays of the form [usage hours, main process crashes, content process crashes, plugin crashes]"""
-    ping_properties = get_pings_properties(pings, comparable_dimensions + [
-        "meta/submissionDate",
-        "creationDate",
-        "payload/info/subsessionLength",
-        "meta/docType",
-        "payload/keyedHistograms/SUBPROCESS_CRASHES_WITH_DUMP/content",
-        "payload/keyedHistograms/SUBPROCESS_CRASHES_WITH_DUMP/plugin",
-        "payload/keyedHistograms/SUBPROCESS_CRASHES_WITH_DUMP/gmplugin",
-    ], with_processes=True) # we only want the parent process data - child processes can also have plugin children, but we want to exclude those
+    def get_property(value, path):
+        for key in path:
+            if key not in value:
+                return None
+            value = value[key]
+        return value
+
+    def get_properties(ping):
+        result = {
+            key: get_property(ping, path)
+            for key, path in zip(dimension_names, comparable_dimensions)
+        }
+        result["submission_date"] = get_property(ping, ("meta", "submissionDate"))
+        result["activity_date"] = get_property(ping, ("creationDate",))
+        result["subsession_length"] = get_property(ping, ("payload", "info", "subsessionLength"))
+        result["doc_type"] = get_property(ping, ("meta", "docType"))
+        result["subprocess_crash_content"] = Histogram("SUBPROCESS_CRASHES_WITH_DUMP", get_property(ping, ("payload", "keyedHistograms", "SUBPROCESS_CRASHES_WITH_DUMP", "content"))).get_value()
+        result["subprocess_crash_plugin"] = Histogram("SUBPROCESS_CRASHES_WITH_DUMP", get_property(ping, ("payload", "keyedHistograms", "SUBPROCESS_CRASHES_WITH_DUMP", "plugin"))).get_value()
+        result["subprocess_crash_gmplugin"] = Histogram("SUBPROCESS_CRASHES_WITH_DUMP", get_property(ping, ("payload", "keyedHistograms", "SUBPROCESS_CRASHES_WITH_DUMP", "gmplugin"))).get_value()
+        return result
+
+    ping_properties = pings.map(get_properties)
+
     def is_valid(ping): # sanity check to make sure the ping is actually usable for our purposes
-        submission_date = ping["meta/submissionDate"]
-        if not isinstance(submission_date, (str, unicode)):
+        if not isinstance(ping["submission_date"], (str, unicode)):
             return False
-        activity_date = ping["creationDate"]
-        if not isinstance(activity_date, (str, unicode)):
+        if not isinstance(ping["activity_date"], (str, unicode)):
             return False
-        subsession_length = ping["payload/info/subsessionLength"]
+        subsession_length = ping["subsession_length"]
         if subsession_length is not None and not isinstance(subsession_length, (int, long)):
             return False
-        build_id = ping["environment/build/buildId"]
-        if not isinstance(build_id, (str, unicode)):
+        if not isinstance(ping["build_id"], (str, unicode)):
             return False
-        if not re.match(r"^\d{14}$", build_id): # only accept valid buildids
+        if not re.match(r"^\d{14}$", ping["build_id"]): # only accept valid buildids
             return False
         return True
 
@@ -78,22 +89,22 @@ def compare_crashes(pings, start_date, end_date, comparable_dimensions, dimensio
         # because we need to normalize and get rid of the time portion
 
         # date the ping was received
-        submission_date = datetime.strptime(ping["meta/submissionDate"], "%Y%m%d").date() # convert the YYYYMMDD format to a real date
+        submission_date = datetime.strptime(ping["submission_date"], "%Y%m%d").date() # convert the YYYYMMDD format to a real date
 
         # date the ping was created on the client
         try:
-            activity_date = dateutil.parser.parse(ping["creationDate"]).date() # the activity date is the date portion of creationDate
+            activity_date = dateutil.parser.parse(ping["activity_date"]).date() # the activity date is the date portion of creationDate
         except: # malformed date, like how sometimes seconds that are not in [0, 59]
             activity_date = submission_date # we'll just set it to the submission date, since we can't parse the activity date
         activity_date = max(submission_date - timedelta(days=7), min(submission_date, activity_date)) # normalize the activity date if it's out of range
 
-        usage_hours = min(25, (ping["payload/info/subsessionLength"] or 0) / 3600.0) # usage hours, limited to ~25 hours to keep things normalized
-        main_crashes = int(ping["meta/docType"] == "crash") # main process crash (is a crash ping)
-        content_crashes = ping["payload/keyedHistograms/SUBPROCESS_CRASHES_WITH_DUMP/content_parent"] or 0
-        plugin_crashes = ping["payload/keyedHistograms/SUBPROCESS_CRASHES_WITH_DUMP/plugin_parent"] or 0
-        gecko_media_plugin_crashes = ping["payload/keyedHistograms/SUBPROCESS_CRASHES_WITH_DUMP/gmplugin_parent"] or 0
+        usage_hours = min(25, (ping["subsession_length"] or 0) / 3600.0) # usage hours, limited to ~25 hours to keep things normalized
+        main_crashes = int(ping["doc_type"] == "crash") # main process crash (is a crash ping)
+        content_crashes = ping["subprocess_crash_content"] or 0
+        plugin_crashes = ping["subprocess_crash_plugin"] or 0
+        gecko_media_plugin_crashes = ping["subprocess_crash_gmplugin"] or 0
         return (
-            (submission_date, activity_date) + tuple(ping[key] for key in comparable_dimensions), # all the dimensions we can compare by
+            (submission_date, activity_date) + tuple(ping[key] for key in dimension_names), # all the dimensions we can compare by
             [ # the crash values
                 1, # number of pings represented by the aggregate
                 usage_hours, main_crashes, content_crashes, plugin_crashes, gecko_media_plugin_crashes,
@@ -102,12 +113,17 @@ def compare_crashes(pings, start_date, end_date, comparable_dimensions, dimensio
                 usage_hours ** 2, main_crashes ** 2, content_crashes ** 2, plugin_crashes ** 2, gecko_media_plugin_crashes ** 2,
             ]
         )
-    crash_values = ping_properties.filter(is_valid).map(get_crash_pair).reduceByKey(lambda a, b: [x + y for x, y in zip(a, b)])
+
+    crash_values = (
+        ping_properties.filter(is_valid)
+                       .map(get_crash_pair)
+                       .reduceByKey(lambda a, b: [x + y for x, y in zip(a, b)])
+    )
 
     def dimension_mapping(pair): # responsible for converting aggregate crash pairs into individual dimension fields
         dimension_key, stats_array = pair
         (submission_date, activity_date), dimension_values = dimension_key[:2], dimension_key[2:]
-        keys = [
+        stats_names = [
             "ping_count",
             "usage_hours", "main_crashes", "content_crashes", "plugin_crashes", "gmplugin_crashes",
             "usage_hours_squared", "main_crashes_squared", "content_crashes_squared", "plugin_crashes_squared", "gmplugin_crashes_squared",
@@ -119,8 +135,9 @@ def compare_crashes(pings, start_date, end_date, comparable_dimensions, dimensio
                 for key, dimension_value in zip(dimension_names, dimension_values)
                 if dimension_value is not None # only include the keys for which the value isn't null
             },
-            dict(zip(keys, stats_array)), # dictionary mapping keys to their corresponding stats
+            dict(zip(stats_names, stats_array)), # dictionary mapping keys to their corresponding stats
         )
+
     return crash_values.map(dimension_mapping)
 
 def retrieve_crash_data(sc, submission_date_range, comparable_dimensions, fraction):
